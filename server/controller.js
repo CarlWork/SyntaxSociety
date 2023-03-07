7231edf7738eb0de00a098a2e74f93b9c6414e3b
CarlWork <carllworks@gmail.com>
2023-03-07 09:17:22 -0600

    NewThumbnail

--- server/controller.js
+++ server/controller.js
@@ -5,7 +5,7 @@ const youtubersData = [
       title: "Sean Allen",
       latestVideoTitle: "Expert Advice to Learn iOS Dev & Swift FAST",
       latestVideoLink: "https://www.youtube.com/watch?v=uWEblkT0_Zk",
-      latestVideoThumbnail: "https://i.ytimg.com/an_webp/uWEblkT0_Zk/mqdefault_6s.webp?du=3000&sqp=CNysmaAG&rs=AOn4CLCAadg8TYtHiEal7-090PGbfLQPSA",
+      latestVideoThumbnail: "https://i.ytimg.com/an_webp/uWEblkT0_Zk/mqdefault_6s.webp?du=3000&sqp=CPKTnaAG&rs=AOn4CLB9XC-PAIX2wNA_C7lGiyODvr2HQA",
       description: "Sean Allen is currently an iOS Developer freelancer, Youtuber, and influencer."
     },
     {
